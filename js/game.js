97aa16a64bf6b49dab110f7dff842b134ec4d360
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-06-07 16:41:33 +0000

    prettierrc file (delete newline at end of file)

--- js/game.js
+++ js/game.js
@@ -1,17 +1,30 @@
 /* global Phaser */
 
-// Created by: xxx
-// Created on: June 2022
+// Copyright (c) 2022 Jaejun Lee All rights reserved
+//
+// Created by: Jaejun Lee
+// Created on: April 2022
 // This is the Phaser3 configuration file
 
-/** Game scene.*/
+//* Game scene*/
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  // set background color
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  //set background color
   backgroundColor: 0x5f6e7a,
-};
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
 
-const game = new Phaser.Game(config);
-console.log(game);
+const game = new Phaser.Game(config)
+console.log(game)
---
+++ js/splashScene.js
@@ -0,0 +1,32 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Jaejun Lee All rights reserved
+//
+// Created by: Jaejun Lee
+// Created on: June 2022
+// This is the Splash Scene
+
+class SplashScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'splashScene'})
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+  }
+
+  creat (data) {
+  }
+
+  update (time, delta) {
+  }
+}
+
+export default SplashScene
+
+
+
