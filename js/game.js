0d64d6e75e27e8110442231e80ca47105b4ea66c
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-06-09 17:28:15 +0000

    title scene

--- js/game.js
+++ js/game.js
@@ -1,17 +1,51 @@
 /* global Phaser */
 
-// Created by: xxx
+// Copyright (c) 2022 Jaejun Lee All rights reserved
+//
+// Created by: Jaejun Lee
 // Created on: June 2022
-// This is the Phaser3 configuration file
+// This is the final project configuration file
 
-/** Game scene.*/
+// scene import statements
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
+
+// create the new scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
   // set background color
-  backgroundColor: 0x5f6e7a,
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
-console.log(game)
+
+// load scenes
+// Note: remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
+
+// the start scene
+game.scene.start("splashScene")
---
+++ js/splashScene.js
@@ -0,0 +1,44 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Jaejun Lee All rights reserved
+//
+// Created by: Jaejun Lee
+// Created on: April 2022
+// This is the Splash Scene
+
+
+/**
+  * This class is the Spash Scene.
+  */
+class SplashScene extends Phaser.Scene {
+
+  constructor () {
+    super({ key: 'splashScene' })
+
+    this.splashSceneBackgroundImage = null
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+    this.load.image("splashSceneBackground" , "./assets/splashSceneImage.png")
+  }
+
+  create (data) {
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, "splashSceneBackground")
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
+  }
+
+  update (time, delta) {
+    if (time > 3000) {
+    this.scene.switch('titleScene')
+    }
+  }
+}
+
+export default SplashScene
+
