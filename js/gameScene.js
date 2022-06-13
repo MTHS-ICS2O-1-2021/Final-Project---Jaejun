aa7339df6b472c047a19f73e6c16d0b17c040d83
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-06-13 17:24:19 +0000

    game background

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,11 +10,11 @@ class GameScene extends Phaser.Scene {
   
   // create an alien
   createAlien() {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1 //spawns the alien between 1 and 1921 pixel
-    let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get number between 1 and 50
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1 //spawns the alien between 1 and 1920 pixel
+    let alienXVelocity = Math.floor(Math.random() * 50) + 50 // this will get number between 1 and 50
     alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign to 50% of cases
     const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
-    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.y = 450
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
   }
@@ -38,7 +38,7 @@ class GameScene extends Phaser.Scene {
     console.log("Game Scene")
 
     // images
-    this.load.image('starBackground', 'assets/starBackground.png')
+    this.load.image('starBackground', 'assets/gameBackground.svg')
     this.load.image('ship', 'assets/spaceShip.png')
     this.load.image('missile', 'assets/missile.png')
     this.load.image('alien', 'assets/alien.png')
@@ -49,7 +49,7 @@ class GameScene extends Phaser.Scene {
   }
   
   create(data) {
-    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(1.0)
     this.background.setOrigin(0, 0)
 
     this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
@@ -89,11 +89,18 @@ class GameScene extends Phaser.Scene {
 
   update(time, delta) {
     //called 60 times a second, hopefully!
+    const keyForwardObj = this.input.keyboard.addKey("W")
+    const keyLeftObj = this.input.keyboard.addKey("A")
+    const keyBackwardObj = this.input.keyboard.addKey("S")
+    const keyRightObj = this.input.keyboard.addKey("D")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
-    const keyLeftObj = this.input.keyboard.addKey('LEFT')
-    const keyRightObj = this.input.keyboard.addKey('RIGHT')
-    const keySpaceObj = this.input.keyboard.addKey('SPACE')
-
+    if (keyForwardObj.isDown === true) {
+      this.ship.y -= 15
+      if (this.ship.y > 1920) {
+        this.ship.y = 1920
+      }
+    }
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
@@ -101,33 +108,20 @@ class GameScene extends Phaser.Scene {
         this.ship.x = 0
       }
     }
+
+    if (keyBackwardObj.isDown === true) {
+      this.ship.y += 15
+      if (this.ship.y > 1920) {
+        this.ship.y = 1920
+      }
+    }
+
     if (keyRightObj.isDown === true) {
       this.ship.x += 15
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
     }
-    
-    if (keySpaceObj.isDown === true) {
-      if (this.fireMissile === false) {
-        // fire missile
-        this.fireMissile = true
-        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
-        this.missileGroup.add(aNewMissile)
-        this.sound.play('laser')
-      }
-    }
-
-    if (keySpaceObj.isUp === true) {
-      this.fireMissile = false
-    }
-
-    this.missileGroup.children.each(function (item) {
-      item.y = item.y - 15
-      if (item.y < 0) {
-        item.destroy()
-      }
-    })
   }
 }
 
