b015c49aa45579c92255d47e654a14110ec3e13a
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-06-14 17:17:19 +0000

    added missile again

--- js/gameScene.js
+++ js/gameScene.js
@@ -3,7 +3,7 @@
 // Copyright (c) 2022 Jaejun Lee All rights reserved
 //
 // Created by: Jaejun Lee
-// Created on: May 2022
+// Created on: June 2022
 // This is the Game Scene
 
 class GameScene extends Phaser.Scene {
@@ -11,10 +11,10 @@ class GameScene extends Phaser.Scene {
   // create an alien
   createAlien() {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1 //spawns the alien between 1 and 1920 pixel
-    let alienXVelocity = Math.floor(Math.random() * 50) + 50 // this will get number between 1 and 50
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get number between 1 and 50
     alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign to 50% of cases
     const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
-    anAlien.body.velocity.y = 450
+    anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
   }
@@ -74,6 +74,8 @@ class GameScene extends Phaser.Scene {
       this.createAlien()
     }.bind(this))
 
+    // Collision between aliens and 
+
     // Collisions between ship and aliens
     this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
       this.sound.play('bomb')
@@ -122,6 +124,26 @@ class GameScene extends Phaser.Scene {
         this.ship.x = 1920
       }
     }
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+        this.missileGroup.add(aNewMissile)
+        this.sound.play('laser')
+      }
+    }
+
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
+    }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
