a46276f2e9f8f4bf9b90d4e9dbe58cd0e71cdc0f
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-06-25 03:07:24 +0000

    added score and new spears

--- js/gameScene.js
+++ js/gameScene.js
@@ -8,7 +8,7 @@
 
 class GameScene extends Phaser.Scene {
   // create an alien
-  createSpear(numSpear) {
+  createSpearTopToBottom(numSpear) {
     for (let i = 0; i < numSpear; i++) {
       const spearXLocation = Math.floor(Math.random() * 1920) + 1
       let spearYVelocity = Math.floor(Math.random() * 100) + 400
@@ -19,7 +19,7 @@ class GameScene extends Phaser.Scene {
       this.spearGroup.add(aSpear)
     }
   }
-  createSpear2(numSpear) {
+  createSpearLeftToRight(numSpear) {
     for (let i = 0; i < numSpear; i++) {
       const spearYLocation = Math.floor(Math.random() * 1080) + 1
       let spearXVelocity = Math.floor(Math.random() * 100) + 400
@@ -31,6 +31,28 @@ class GameScene extends Phaser.Scene {
     }
   }
 
+  createSpearRightToLeft(numSpear) {
+    for (let i = 0; i < numSpear; i++) {
+      const spearYLocation = Math.floor(Math.random() * 1080) + 1
+      let spearXVelocity = Math.floor(Math.random() * 100) + 400
+      spearXVelocity *= Math.round(Math.random()) ? 1 : -1
+      const aSpear = this.physics.add.sprite(1920, spearYLocation, "spearRight")
+      aSpear.body.velocity.x = -spearXVelocity
+      aSpear.body.velocity.y = 0
+      this.spearGroup2.add(aSpear)
+    }
+  }
+  createSpearBottomToTop(numSpear) {
+    for (let i = 0; i < numSpear; i++) {
+      const spearXLocation = Math.floor(Math.random() * 1920) + 1
+      let spearYVelocity = Math.floor(Math.random() * 100) + 400
+      spearYVelocity *= Math.round(Math.random()) ? 1 : -1
+      const aSpear = this.physics.add.sprite(spearXLocation, 1080, "spearBottom")
+      aSpear.body.velocity.y = -spearYVelocity
+      aSpear.body.velocity.x = 0
+      this.spearGroup2.add(aSpear)
+    }
+  }
   constructor() {
     super({ key: "gameScene" })
 
@@ -61,6 +83,8 @@ class GameScene extends Phaser.Scene {
     this.load.image("knight", "assets/knight.png")
     this.load.image("spearTop", "assets/spearTop.png")
     this.load.image("spearLeft", "assets/spearLeft.png")
+    this.load.image("spearRight", "assets/spearRight.png")
+    this.load.image("spearBottom", "assets/spearBottom.png")
   }
   create(data) {
     this.background = this.add.image(0, 0, "background").setScale(1.0)
@@ -77,8 +101,14 @@ class GameScene extends Phaser.Scene {
 
     // create a group of the aliens
     this.spearGroup = this.add.group()
-    this.createSpear(10)
-    this.createSpear2(10)
+    this.spearGroup2 = this.add.group()
+    this.createSpearTopToBottom(10)
+    this.createSpearLeftToRight(10)
+    this.createSpearRightToLeft(10)
+    this.createSpearBottomToTop(10)
+
+    this.gameOver = false
+    this.score = 0 // reset the score
 
     // Collisions between knight and spears
     this.physics.add.collider(
@@ -98,17 +128,44 @@ class GameScene extends Phaser.Scene {
           .setOrigin(0.5)
         this.gameOverText.setInteractive({ useHandCursor: true })
         this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
+        this.gameOver = true
+      }.bind(this)
+    )
+    this.physics.add.collider(
+      this.knight,
+      this.spearGroup2,
+      function (knightCollide, spearCollide) {
+        this.physics.pause()
+        spearCollide.destroy()
+        knightCollide.destroy()
+        this.gameOverText = this.add
+          .text(
+            1920 / 2,
+            1080 / 2,
+            "Game Over!\nClick to play again.",
+            this.gameOverTextStyle
+          )
+          .setOrigin(0.5)
+        this.gameOverText.setInteractive({ useHandCursor: true })
+        this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
+        this.gameOver = true
       }.bind(this)
     )
   }
+ 
 
   update(time, delta) {
-    //called 60 times a second, hopefully!
+
     const keyForwardObj = this.input.keyboard.addKey("UP")
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyBackwardObj = this.input.keyboard.addKey("DOWN")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
 
+    if (!this.gameOver) {
+      this.score = this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
+    }
+    
     if (keyForwardObj.isDown === true) {
       this.knight.y -= 10
       if (this.knight.y < 1) {
@@ -145,6 +202,17 @@ class GameScene extends Phaser.Scene {
 
       if (item.x > 1920) {
         item.x = 0
+        item.y = Math.floor(Math.random() * 1080) + 1
+      }
+    })
+    this.spearGroup2.children.each(function (item) {
+      if (item.y < 0) {
+        item.y = 1080
+        item.x = Math.floor(Math.random() * 1920) + 1
+      }
+
+      if (item.x < 0) {
+        item.x = 1920
         item.y = Math.floor(Math.random() * 1080) + 1
       }
     })
